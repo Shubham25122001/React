e53868916be43d1745e1e4a7a40a09f0953cd424
shubham <shubham25122001@gmail.com>
2023-02-08 10:29:01 +0530

    _

--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -3,15 +3,17 @@ import React, {useState} from 'react';
 
 export default function TextForm(props) {
     const handleUpClick =()=>{
-        console.log("Uppercase was clicked");
-        setText("You have clicked on handleUpClick");
+        // console.log("Uppercase was clicked"+ text);
+        let newText = text.toUpperCase();
+        setText(newText);
     }
-    const handleOnChange =()=>{
-        console.log("On change");
+    const handleOnChange =(event)=>{
+        // console.log("On change");
+        setText(event.target.value);
     }
 
     const [text, setText] = useState("Enter text here");
-    //setText("Mew text");
+    // setText("Mew text");
     return (
         <div>
         <h1>{props.heading}</h1>
